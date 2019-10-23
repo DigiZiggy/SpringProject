4bb6f4a7d4614864b1b1bde34930dca900c2f9b8
Sigrid Narep <sigridnarep@Sigrids-MacBook-Air-2.local>
2019-10-23 21:08:23 +0300

    Remove import ..bind.annotation.* , add all imports separately in order to make PMD happy

--- src/main/java/app/OrderController.java
+++ src/main/java/app/OrderController.java
@@ -2,7 +2,12 @@ package app;
 
 import model.Order;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
 import java.util.List;
--- src/main/java/app/ValidationController.java
+++ src/main/java/app/ValidationController.java
@@ -3,7 +3,9 @@ package app;
 import model.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import validation.ValidationErrors;
 
 import javax.validation.ConstraintViolation;
